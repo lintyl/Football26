4953ba42e2ad71e4a114c1dffc0f9fbf7a8cfa8a
Tyler Lin <lintyl@bc.edu>
2025-08-31 23:44:11 -0400

    Initial Commit

---
+++ WhatIsFootball/WhatIsFootballApp.swift
@@ -0,0 +1,17 @@
+//
+//  WhatIsFootballApp.swift
+//  WhatIsFootball
+//
+//  Created by Tyler Lin on 8/31/25.
+//
+
+import SwiftUI
+
+@main
+struct WhatIsFootballApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
